d074d044c0a4cecc5d78dcf6ac2e1085b3764df1
yichunli95 <yichunli2019@u.northwestern.edu>
2018-11-05 19:36:39 -0600

    Update for double save and custom filenames
    Update to save one copy of file and take in custom file names

--- QuicktimeAutomation.py
+++ QuicktimeAutomation.py
@@ -1,8 +1,9 @@
 from subprocess import Popen, PIPE
 
 app = "Terminal"
-
+file = "newRecording.mov"
 record = '''
+    set fileName to (path to desktop as text) & "{0}"
     tell application "QuickTime Player"
         activate
         set sr to new screen recording
@@ -10,15 +11,14 @@ record = '''
         repeat while exists sr
             delay 1
         end repeat
-        set fileName to (path to desktop as text) & "newRecording.mov"
         open for access file fileName
-        tell last item of documents
+        tell first document
             export in fileName using settings preset "720p"
-            close
+            close saving no
         end tell
-        return fileName
     end tell
-  ''' 
+    return fileName
+  '''.format(file)
 
 proc = Popen(['osascript', '-'], stdin=PIPE, stdout=PIPE, stderr=PIPE, universal_newlines=True)
 result, error = proc.communicate(record)
